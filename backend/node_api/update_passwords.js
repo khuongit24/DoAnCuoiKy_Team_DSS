e7c2588d6a4a1b5f2f8815fad5044fc64887daed
Em Khương <161446361+khuongit24@users.noreply.github.com>
2026-07-20 16:33:57 +0700

    Delete backend/node_api directory

--- backend/node_api/update_passwords.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const db = require('./src/config/database');
-const HASH = '$2b$12$hOFUjhdgBwryGK4.Ff.O6uwPfV07Xn7wiYa1Ek87E4GCS9LZnUHC6';
-db.query('UPDATE users SET password_hash = $1', [HASH])
-  .then(res => { 
-    console.log('Updated', res.rowCount, 'users'); 
-    process.exit(0); 
-  })
-  .catch(console.error);
